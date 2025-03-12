33f403d2fbb050ed3373ee523923c8dbb2c8e11b
Benjosm <92320536+Benjosm@users.noreply.github.com>
2025-03-12 11:26:03 -0500

    feat: Set up basic Express server

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,10 @@
 // src/app.ts
 import express from 'express';
-import bodyParser from 'body-parser';
-import issueRoutes from './routes/issueRoutes';
 
 const app = express();
 
-app.use(bodyParser.json());
-
-app.use('/issues', issueRoutes);
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
 
 export default app;
