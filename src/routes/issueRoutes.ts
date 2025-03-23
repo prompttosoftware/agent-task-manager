6bd9c9f6949fd7971551097c25e5ac3fb7142980
Benjosm <92320536+Benjosm@users.noreply.github.com>
2025-03-23 09:34:22 -0500

    Cleanup

--- src/routes/issueRoutes.ts
+++
@@ -1,9 +0,0 @@
-// src/routes/issueRoutes.ts
-import express from 'express';
-import { getIssueCreateMeta } from '../controllers/issueCreateMetaController';
-
-const router = express.Router();
-
-router.get('/issue/createmeta', getIssueCreateMeta);
-
-export default router;
