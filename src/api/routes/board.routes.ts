670edc2f1eccb51f0378c6ba3b4a2bd3b087a856
Benjosm <92320536+Benjosm@users.noreply.github.com>
2025-04-03 20:27:54 -0500

    ATM-990: Design and Implement DELETE /boards/:boardId endpoint

--- src/api/routes/board.routes.ts
+++ src/api/routes/board.routes.ts
@@ -1,7 +1,14 @@
 // src/api/routes/board.routes.ts
-import express from 'express';
-const router = express.Router();
+import { Router } from 'express';
+import { body, param } from 'express-validator';
+import * as boardController from '../controllers/board.controller';
 
-// Define routes here
+const router = Router();
+
+// GET /api/boards/:boardId
+// router.get('/:boardId', boardController.getBoard);
+
+// DELETE /api/boards/:boardId
+router.delete('/:boardId', [param('boardId').isInt().withMessage('Invalid board ID')], boardController.deleteBoard);
 
 export default router;
