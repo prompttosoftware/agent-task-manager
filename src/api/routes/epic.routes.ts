b699b16cf5061134bc133ac302937be4d6b2c696
Benjosm <92320536+Benjosm@users.noreply.github.com>
2025-04-04 06:57:57 -0500

    ATM-895: Epic 13.6: Implement DELETE /epics/:epicKey route handler

--- src/api/routes/epic.routes.ts
+++ src/api/routes/epic.routes.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
-import { getEpics } from '../controllers/epic.controller';
+import { getEpics, deleteEpic } from '../controllers/epic.controller';
 
 const router = Router();
 
 router.get('/epics', getEpics);
+router.delete('/epics/:epicKey', deleteEpic);
 
 export default router;
