f72785ebb347564711ede47d0e06a0f6d54c32a9
Benjosm <92320536+Benjosm@users.noreply.github.com>
2025-04-14 09:16:34 -0500

    ATM-988: Design and Implement POST /boards endpoint

--- src/api/controllers/board.controller.ts
+++ src/api/controllers/board.controller.ts
@@ -1,12 +1,23 @@
 import { Request, Response } from 'express';
-import { getAllBoards } from '../services/board.service';
+import { validationResult } from 'express-validator';
+import * as boardService from '../api/services/board.service';
+import { CreateBoardData } from '../types/board';
 
-export const getBoards = async (req: Request, res: Response) => {
+export const createBoard = async (req: Request, res: Response) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
   try {
-    const boards = await getAllBoards();
-    res.json(boards);
+    const { name, description } = req.body as CreateBoardData;
+    const newBoard = await boardService.createBoard({
+      name,
+      description,
+    });
+    res.status(201).json(newBoard);
   } catch (error: any) {
-    console.error('Error fetching boards:', error);
-    res.status(500).json({ error: 'Failed to fetch boards' });
+    console.error('Error creating board in controller:', error);
+    res.status(500).json({ message: 'Failed to create board' });
   }
 };
