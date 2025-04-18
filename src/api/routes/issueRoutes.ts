3714b9d92b98a4245bc01bff44c548d073552c38
Benjosm <92320536+Benjosm@users.noreply.github.com>
2025-04-17 19:18:04 -0500

    ATM-1124: Implement updateIssue endpoint

--- src/api/routes/issueRoutes.ts
+++ src/api/routes/issueRoutes.ts
@@ -3,9 +3,9 @@ import { issueController } from '../controllers/issueController';
 
 const router = express.Router();
 
-router.post('/link', issueController.linkIssues);
 router.post('/', issueController.createIssue);
-router.get('/search', issueController.searchIssues);
 router.get('/:issueIdOrKey', issueController.getIssue);
+router.put('/:issueIdOrKey', issueController.updateIssue);
+router.delete('/:issueIdOrKey', issueController.deleteIssue);
 
 export default router;
