2ad6bdc523d9c1aecc4af7423c144f1df81d2ae9
Benjosm <92320536+Benjosm@users.noreply.github.com>
2025-05-13 07:17:32 -0500

    Reset

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('ts-jest').JestConfigTsJest} */
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  // Add moduleNameMapper for path resolution
-  moduleNameMapper: {
-    '^src/(.*)$': '<rootDir>/dist/src/$1',
-  }
-};
