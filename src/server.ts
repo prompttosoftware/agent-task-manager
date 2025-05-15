85b52cd105ca4a35806e363ff37fc1eb2bf3394b
Benjosm <92320536+Benjosm@users.noreply.github.com>
2025-05-15 17:46:48 -0500

    ATM-1289: Project Setup

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
-import app from './app'; // Assuming app is the default export from app.ts
+import app from './app';
 
-const port = process.env.PORT || 3000; // Use environment variable for port or default to 3000
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
