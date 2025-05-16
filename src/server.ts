81d604502f2cdf6a96409d65da30d08ee7a23650
Benjosm <92320536+Benjosm@users.noreply.github.com>
2025-05-16 09:54:25 -0500

    Reset

--- src/server.ts
+++
@@ -1,8 +0,0 @@
-import app from './app'; // Assuming app is the default export from app.ts
-
-const port = process.env.PORT || 3000; // Use environment variable for port or default to 3000
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-  console.log(`Access the application at http://localhost:${port}`);
-});
