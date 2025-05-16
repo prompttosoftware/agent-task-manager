f6ed2bceffff381f95dd4378e267d8266ad46f17
Benjosm <92320536+Benjosm@users.noreply.github.com>
2025-05-16 13:09:13 -0500

    ATM-1289: Project Setup

--- src/app.ts
+++ src/app.ts
@@ -2,16 +2,11 @@ import express from 'express';
 import { v4 as uuidv4 } from 'uuid';
 
 const app = express();
-const port = 3000;
 
 app.use(express.json());
 
 app.get('/', (req, res) => {
- res.send('Hello World!');
-});
-
-app.listen(port, () => {
- console.log(`Server is running on port ${port}`);
+  res.send('Hello World!');
 });
 
 export default app;
