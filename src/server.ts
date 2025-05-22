ea369e1cc1dfc2cf56106aa02e964431e28fc9e8
Benjosm <92320536+Benjosm@users.noreply.github.com>
2025-05-22 18:16:03 -0500

    ATM-1290: Core Data Models

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,22 @@
-import './app';
+import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+import swaggerJsdoc from 'swagger-jsdoc';
+import apiRouter from './api/index';
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Jira Clone API',
+      version: '1.0.0',
+    },
+  },
+  apis: ['./src/api/*.ts', './src/models/*.ts'],
+};
+
+const specs = swaggerJsdoc(options);
+
+const app = express();
+app.use(express.json());
+app.use('/api', apiRouter);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
