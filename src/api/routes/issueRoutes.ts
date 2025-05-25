ac70ff4354ead69b403a1b4868a33277d2dfd57e
Benjosm <92320536+Benjosm@users.noreply.github.com>
2025-05-25 11:01:08 -0500

    Reset

--- src/api/routes/issueRoutes.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import * as issueService from '../../services/issueService.ts';
-import { createIssue } from '../controllers/issueController.ts';
-
-const router = express.Router();
-
-router.post('/rest/api/2/issue', createIssue);
-
-export const issueRoutes = router;
