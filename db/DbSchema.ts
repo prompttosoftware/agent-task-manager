1167f9467eff2abea55fd0be102db49e7835d9b7
Benjosm <92320536+Benjosm@users.noreply.github.com>
2025-06-04 09:07:13 -0500

    Reset

--- db/DbSchema.ts
+++
@@ -1,8 +0,0 @@
-import { AnyIssue } from '../models';
-
-export const DB_FILE_PATH = "/usr/src/agent-task-manager/.data/db.json";
-
-export interface DbSchema {
-  issues: AnyIssue[];
-  issueKeyCounter: number;
-}
