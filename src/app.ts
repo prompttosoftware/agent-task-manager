0aeabcf42c4273d690afb5b128f6baf3d02c39d7
Benjosm <92320536+Benjosm@users.noreply.github.com>
2025-06-04 15:08:39 -0500

    ATM-1295: Implement API Route and Controller Setup

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,11 @@
 import express, { Express, Request, Response } from 'express';
+import issueRoutes from './api/routes/issueRoutes'; // Import issue routes
 
 const app: Express = express();
 const port: number = 3000;
+
+// Mount issue routes
+app.use('/', issueRoutes); // Use the issue routes
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
