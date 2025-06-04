4b7bd935ea8e180b82fec76c182b04cc2dece698
Benjosm <92320536+Benjosm@users.noreply.github.com>
2025-06-03 21:09:39 -0500

    ATM-1304: Create issueController.ts

--- src/api/routes/issueRoutes.test.ts
+++ src/api/routes/issueRoutes.test.ts
@@ -1,13 +1,15 @@
 import request from 'supertest';
-import app from '../../app';
+import app from '../../src/app';
+import * as issueController from './controllers/issueController';
+import { Request, Response, NextFunction } from 'express';
 
-describe('POST /rest/api/2/issue', () => {
-  it('should return 200 with a success message', async () => {
-    const response = await request(app)
-      .post('/rest/api/2/issue')
-      .send({});
+describe('Issue Routes', () => {
+  it('should create a new issue', async () => {
+    jest.spyOn(issueController, 'createIssue').mockImplementation((req: Request, res: Response, next: NextFunction) => {
+      res.sendStatus(201);
+    });
 
-    expect(response.statusCode).toBe(200);
-    expect(response.body.message).toBe('Issue created successfully!');
+    const response = await request(app).post('/rest/api/2/issues');
+    expect(response.statusCode).toBe(201);
   });
 });
