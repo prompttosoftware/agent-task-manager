fa2f73ebdec472176cc2205e2a3327bc8357d94f
Benjosm <92320536+Benjosm@users.noreply.github.com>
2025-06-04 18:15:52 -0500

    Reset

--- src/index.ts
+++
@@ -1,4 +0,0 @@
-import server from './server';
-
-// The server is started when imported due to the top-level app.listen call in server.ts
-// This file serves purely as the entry point
--- src/server.ts
+++
@@ -1,9 +0,0 @@
-import app from './app';
-
-const port = process.env.PORT || 3000;
-
-const server = app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
-
-export default server;
