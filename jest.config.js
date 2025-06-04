6f51db4ed0146f7bda4ba366ed4f722ea8b5e20f
Benjosm <92320536+Benjosm@users.noreply.github.com>
2025-06-04 18:52:38 -0500

    Reset

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testMatch: ['<rootDir>/src/**/*.test.ts', '<rootDir>/models/**/*.test.ts'],
-  verbose: true,
-  forceExit: true,
-};
