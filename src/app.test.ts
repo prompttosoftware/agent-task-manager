0cfcf6cfb1ac2ddece214caad40ce13a9cf5c517
Benjosm <92320536+Benjosm@users.noreply.github.com>
2025-06-03 19:34:32 -0500

    Reset

--- src/app.test.ts
+++
@@ -1,5 +0,0 @@
-describe('Basic test', () => {
-  it('should check if 1 + 1 equals 2', () => {
-    expect(1 + 1).toBe(2);
-  });
-});
--- src/server.ts
+++
@@ -1,7 +0,0 @@
-import app from './app';
-
-const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
-
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
