c54b7dff009ecdded5f81ec7817f3e22d2978ac7
Benjosm <92320536+Benjosm@users.noreply.github.com>
2025-06-05 08:10:04 -0500

    Reset

--- src/api/controllers/issueController.ts
+++
@@ -1,8 +0,0 @@
-// src/api/controllers/issueController.ts
-
-import { Request, Response } from 'express';
-
-export const createIssue = (req: Request, res: Response) => {
-  // TODO: Implement issue creation logic here
-  res.status(201).json({ message: 'Issue created successfully' });
-};
