eda7a2608d8725fbab0207a79f164e155b47feb7
Benjosm <92320536+Benjosm@users.noreply.github.com>
2025-06-04 20:25:38 -0500

    Reset

--- src/server.ts
+++
@@ -1,8 +0,0 @@
-import app from './app'; // Assuming src/app.ts exports the Express app instance
-
-const port = 3000; // Define the port number
-
-// Start the server
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
