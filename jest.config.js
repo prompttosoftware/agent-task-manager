95672f189ab317db51ea571eb534b04257607c4f
Benjosm <92320536+Benjosm@users.noreply.github.com>
2025-06-06 11:21:21 -0500

    Reset

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  rootDir: '.',
-  testMatch: ['<rootDir>/src/**/*test.(ts|tsx|js)'],
-};
