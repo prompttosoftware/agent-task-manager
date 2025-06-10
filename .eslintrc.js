cfe79d3a1094966d8d6591765192c1efa48fe5d3
Benjosm <92320536+Benjosm@users.noreply.github.com>
2025-06-09 20:52:23 -0500

    ATM-1428: Initialize Node.js Monorepo with Core Dependencies and Configuration

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: ['./tsconfig.json'],
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  rules: {
+    // Add your custom rules here, or override the recommended ones
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    'no-console': 'warn',
+  },
+  ignorePatterns: ['dist', 'node_modules'],
+};
