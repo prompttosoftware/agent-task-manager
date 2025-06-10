4900c33383c62e19ae7158b93824a33ea4d6f774
Benjosm <92320536+Benjosm@users.noreply.github.com>
2025-06-09 19:30:59 -0500

    ATM-1413: Create `issue.routes.ts` and Define Issue API Routes

---
+++ src/routes/issue.routes.ts
@@ -0,0 +1,45 @@
+import { Router, Request, Response, NextFunction } from 'express';
+import { IssueController } from '../controllers/issue.controller';
+import logger from '../utils/logger';
+
+const router = Router();
+const issueController = new IssueController();
+
+// Placeholder for AuthMiddleware - Assuming it will be used for authentication
+// import { AuthMiddleware } from '../middleware/auth.middleware';
+// const authMiddleware = new AuthMiddleware();
+
+// POST /rest/api/2/issue - Create Issue
+router.post('/rest/api/2/issue', /*authMiddleware.authenticate,*/ async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        logger.info('POST /rest/api/2/issue called');
+        await issueController.createIssue(req, res, next);
+    } catch (error) {
+        logger.error('Error creating issue:', error);
+        next(error);
+    }
+});
+
+// GET /rest/api/2/issue/{issueKey} - Get Issue by Key
+router.get('/rest/api/2/issue/:issueKey', /*authMiddleware.authenticate,*/ async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        logger.info('GET /rest/api/2/issue/:issueKey called');
+        await issueController.getIssue(req, res, next);
+    } catch (error) {
+        logger.error('Error getting issue:', error);
+        next(error);
+    }
+});
+
+// DELETE /rest/api/2/issue/{issueKey} - Delete Issue by Key
+router.delete('/rest/api/2/issue/:issueKey', /*authMiddleware.authenticate,*/ async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        logger.info('DELETE /rest/api/2/issue/:issueKey called');
+        await issueController.deleteIssue(req, res, next);
+    } catch (error) {
+        logger.error('Error deleting issue:', error);
+        next(error);
+    }
+});
+
+export { router as issueRoutes };
